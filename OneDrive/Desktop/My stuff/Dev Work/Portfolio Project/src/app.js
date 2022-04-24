1342bde9637f92da522af6d708ae8eb90e0e43f3
PizzasaurusR <99826078+PizzasaurusR@users.noreply.github.com>
2022-04-25 01:28:45 +0200

    Add files via upload

---
+++ OneDrive/Desktop/My stuff/Dev Work/Portfolio Project/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("/public"));
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname +"/index.html");
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+})
